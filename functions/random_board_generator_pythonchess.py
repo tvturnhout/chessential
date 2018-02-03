cbd105ae379fa2638d5f0a61b62f8104c8a01b56
TvT <tvturnhout@gmail.com>
2018-02-03 19:56:47 +0100

    added random moves

--- functions/random_board_generator_pythonchess.py
+++ functions/random_board_generator_pythonchess.py
@@ -1,8 +1,17 @@
 import chess
 import random
 
-board = chess.Board()
-legal_moves = board.legal_moves
-move = random.choice(legal_moves)
 
-print (move)
+
+board = chess.Board()
+print(board)
+
+legal_moves = board.legal_moves
+potential_moves = []
+for item in legal_moves:
+    potential_moves.append(item)
+
+move = random.choice(potential_moves)
+print(move)
+board.push(move)
+print(board)
