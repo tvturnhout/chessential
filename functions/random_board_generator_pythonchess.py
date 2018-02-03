da8ebe236b1314fd9d9e138a80947974ffb7ad33
TvT <tvturnhout@gmail.com>
2018-02-03 20:14:50 +0100

    added loop to generate multiple boards

--- functions/random_board_generator_pythonchess.py
+++ functions/random_board_generator_pythonchess.py
@@ -6,12 +6,28 @@ import random
 board = chess.Board()
 print(board)
 
-legal_moves = board.legal_moves
-potential_moves = []
-for item in legal_moves:
-    potential_moves.append(item)
+boards = []
+i = 0
+while not i > 50 or board.is_game_over() or board.is_insufficient_material() or board.is_stalemate() or board.is_seventyfive_moves():
 
-move = random.choice(potential_moves)
-print(move)
-board.push(move)
-print(board)
+    legal_moves = board.legal_moves
+    potential_moves = []
+    for item in legal_moves:
+        potential_moves.append(item)
+
+    i = i + 1 
+    move = random.choice(potential_moves)
+    board.push(move)
+    boards.append(board)
+    print(move)
+    #print(board)
+    print(len(boards))
+
+outF = open("myOutFile.txt", "w")
+for line in boards:
+  outF.write(line)
+  outF.write("\n")
+outF.close()
+
+
+
