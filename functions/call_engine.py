775b3f42f854defd34a9f83bb0488eddb0504e86
TvT <tvturnhout@gmail.com>
2018-02-03 22:14:47 +0100

    engine call function

---
+++ functions/call_engine.py
@@ -0,0 +1,13 @@
+import chess.uci
+import numpy as np
+import chess
+
+def best_board(input_board, think_time):
+
+    engine = chess.uci.popen_engine("./../stockfish-9-win/Windows/stockfish_9_x64.exe")
+    board = input_board
+    engine.position(board)
+    board.push(engine.go(ponder=False , movetime = think_time)[0])
+    engine.quit()
+
+    return(board)
