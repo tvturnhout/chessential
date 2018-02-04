2695f0fc7c7a18ec07f91bcb78c2215a31bc0388
janssenswillen <janssenswillen@ibspe.com>
2018-02-04 02:38:16 +0100

    Ran model

--- machine_learning/evaluation.py
+++ machine_learning/evaluation.py
@@ -16,8 +16,6 @@ X, y = readdata('./../data/20180204T0021boards.h5')
 
 predicted_boards = predict(X)
 
-print(predicted_boards[5])
-print(predicted_boards[15])
 print(vector2string(predicted_boards[5]))
 
 print(vector2string(predicted_boards[10]))
