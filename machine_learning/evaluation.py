6f5773848475dd0f7887889d20dce126f42fe7d0
TvT <tvturnhout@gmail.com>
2018-02-04 02:20:58 +0100

    douwe

--- machine_learning/evaluation.py
+++ machine_learning/evaluation.py
@@ -15,22 +15,13 @@ import chess
 X, y = readdata('./../data/20180204T0021boards.h5')
 
 predicted_boards = predict(X)
-i = 0
-error = 0
-while i < len(predicted_boards):
-    #print(predicted_boards[random.randint(0,100)])
-    try:
-        vector2string(predicted_boards[i])
-    except:
-        error = error + 1
-        if error % 100 == 0:
-            print(str(error))
-            print('---')
-            print(str(i))
-        #print('Al ' + str(error) + ' kromme borden van de ' + str(i) ' gecheckte borden gevonden')
-        
 
-print('Aantal kromme borden: ' + str(error) + ' van totaal ' + str(len(predicted_boards)) + ' borden')
+print(predicted_boards[5])
+print(predicted_boards[15])
+print(vector2string(predicted_boards[5]))
+
+print(vector2string(predicted_boards[10]))
+
 
 
 
