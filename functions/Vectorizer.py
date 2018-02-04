4e8c6be6a05d30dd0d3f94b37c22a5536e5f0510
janssenswillen <janssenswillen@ibspe.com>
2018-02-04 01:58:30 +0100

    2 string
    Signed-off-by: janssenswillen <janssenswillen@ibspe.com>

--- functions/Vectorizer.py
+++ functions/Vectorizer.py
@@ -50,6 +50,28 @@ def vector2board(Vector):
 #            board_asString +=  '\n' + board_asString2[it]
     return chess.Board(board_asString3)
 
+def vector2string(Vector):
+    board_asString2 = ''
+    for it in range(0,len(Vector),len(PossibleEntries)):
+        board_asString2 += PossibleEntries[int(np.where(Vector[it:it + 13])[0])]
+    board_asString3 = ''
+    r = 0
+    for p in board_asString2:
+        r += 1
+        board_asString3 += p
+        if r is 8:
+            board_asString3 += '\n'
+            r=0
+
+#
+#    board_asString = board_asString2[0]
+#    for it in range(1,len(board_asString2)):
+#        if it % 8>0:
+#            board_asString +=  ' ' + board_asString2[it]
+#        else:
+#            board_asString +=  '\n' + board_asString2[it]
+    return board_asString3
+
 if __name__ is '__main__':
     Board = chess.Board()
     start = time.time()
@@ -63,6 +85,7 @@ if __name__ is '__main__':
     for i in range(number):
         Board = vector2board(Vector)
     end = time.time()
+    print(vector2string(Vector))
     print('Took me {} ms per board'.format((end-start)/number*1e3))
 
 
